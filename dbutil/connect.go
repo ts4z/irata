5b322be765c38b39d41a6ff79a18008bf3ad16d6
Tim Showalter <tjs@psaux.com>
2025-11-15 15:17:15 -0800

    Remove the GCP SQL connector.

--- dbutil/connect.go
+++ dbutil/connect.go
@@ -1,91 +1,14 @@
 package dbutil
 
 import (
-	"context"
 	"database/sql"
 	"errors"
-	"fmt"
 	"log"
 	"maps"
-	"net"
-	"os"
 	"slices"
 
-	"cloud.google.com/go/cloudsqlconn"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/ts4z/irata/config"
 )
-
-type cloudEnvSettings struct {
-	dbUser,
-	dbPwd,
-	dbName,
-	instanceConnectionName,
-	usePrivate string
-}
-
-func (s *cloudEnvSettings) getenv() error {
-	unset := []string{}
-	getenv := func(k string) string {
-		v := os.Getenv(k)
-		if v == "" {
-			unset = append(unset, k)
-		}
-		return v
-	}
-
-	s.dbUser = getenv("DB_USER")                                  // e.g. 'my-db-user'
-	s.dbPwd = getenv("DB_PASS")                                   // e.g. 'my-db-password'
-	s.dbName = getenv("DB_NAME")                                  // e.g. 'my-database'
-	s.instanceConnectionName = getenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
-	s.usePrivate = os.Getenv("PRIVATE_IP")
-
-	if unset != nil {
-		return fmt.Errorf("cloudsqlconn: unset variables: %+v", unset)
-	}
-	return nil
-}
-
-func connectWithConnector() (*sql.DB, error) {
-	// Note: Saving credentials in environment variables is convenient, but not
-	// secure - consider a more secure solution such as
-	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-	// keep passwords and other secrets safe.
-
-	env := &cloudEnvSettings{}
-	env.getenv()
-
-	dsn := fmt.Sprintf("user=%s password=%s database=%s", env.dbUser, env.dbPwd, env.dbName)
-	config, err := pgx.ParseConfig(dsn)
-	if err != nil {
-		return nil, err
-	}
-	var opts []cloudsqlconn.Option
-	if env.usePrivate != "" {
-		opts = append(opts, cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()))
-	}
-	// WithLazyRefresh() Option is used to perform refresh
-	// when needed, rather than on a scheduled interval.
-	// This is recommended for serverless environments to
-	// avoid background refreshes from throttling CPU.
-	opts = append(opts, cloudsqlconn.WithLazyRefresh())
-	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
-	if err != nil {
-		return nil, err
-	}
-	// Use the Cloud SQL connector to handle connecting to the instance.
-	// This approach does *NOT* require the Cloud SQL proxy.
-	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
-		return d.Dial(ctx, env.instanceConnectionName)
-	}
-	dbURI := stdlib.RegisterConnConfig(config)
-	dbPool, err := sql.Open("pgx", dbURI)
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %w", err)
-	}
-	return dbPool, nil
-}
 
 func connectWithPgx() (*sql.DB, error) {
 	url := config.DBURL()
@@ -96,10 +19,15 @@ func connectWithPgx() (*sql.DB, error) {
 	return sql.Open("pgx", url)
 }
 
+// Connect establishes a database connection using the configured SQL connector.
+//
+// A previouis version of this code supported the Google Cloud connector.  That
+// code neds some work (it shouldn't need a password, but it did) and has been
+// removed for now.  However, it increased the binary size by about 10MB to pull in
+// the various GCP libraries.  Since we're not using it, better not have it.
 func Connect() (*sql.DB, error) {
 	factories := map[string]func() (*sql.DB, error){
-		"connector": connectWithConnector,
-		"pgx":       connectWithPgx,
+		"pgx": connectWithPgx,
 	}
 	factory, ok := factories[config.SQLConnector()]
 	if !ok {
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/ts4z/irata
 go 1.25.2
 
 require (
-	cloud.google.com/go/cloudsqlconn v1.18.1
 	github.com/gorilla/securecookie v1.1.2
 	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/jackc/pgx/v5 v5.7.6
@@ -17,45 +16,23 @@ require (
 )
 
 require (
-	cloud.google.com/go/auth v0.16.4 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
-	cloud.google.com/go/compute/metadata v0.8.0 // indirect
-	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/go-logr/logr v1.4.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
-	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/google/s2a-go v0.1.9 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
-	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.62.0 // indirect
-	go.opentelemetry.io/otel v1.37.0 // indirect
-	go.opentelemetry.io/otel/metric v1.37.0 // indirect
-	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
-	golang.org/x/time v0.12.0 // indirect
-	google.golang.org/api v0.247.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250811230008-5f3141c8851a // indirect
-	google.golang.org/grpc v1.74.2 // indirect
-	google.golang.org/protobuf v1.36.7 // indirect
 )
